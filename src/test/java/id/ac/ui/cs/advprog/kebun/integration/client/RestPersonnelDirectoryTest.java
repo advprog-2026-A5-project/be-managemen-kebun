cf4838da3d555c548b766d7e5584b32ccf4b0e78
Matthew Wijaya <matthew.wijaya@ui.ac.id>
2026-05-21 15:22:51 +0700

    refactor: strengthen personnel directory token contract tests

--- src/test/java/id/ac/ui/cs/advprog/kebun/integration/client/RestPersonnelDirectoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/kebun/integration/client/RestPersonnelDirectoryTest.java
@@ -9,7 +9,9 @@ import org.springframework.web.client.RestClient;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.header;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
@@ -24,11 +26,13 @@ class RestPersonnelDirectoryTest {
 
         AuthServiceProperties properties = new AuthServiceProperties();
         properties.setBaseUrl("http://auth-service");
+        properties.setInternalServiceToken("token-123");
 
         RestPersonnelDirectory directory = new RestPersonnelDirectory(builder, properties);
 
         server.expect(requestTo("http://auth-service/internal/users/7/identity"))
                 .andExpect(method(HttpMethod.GET))
+                .andExpect(header("X-Internal-Service-Token", "token-123"))
                 .andRespond(withSuccess("""
                         {"id":7,"email":"mandor@mysawit.id","nama":"Mandor","role":"MANDOR"}
                         """, MediaType.APPLICATION_JSON));
@@ -46,11 +50,13 @@ class RestPersonnelDirectoryTest {
 
         AuthServiceProperties properties = new AuthServiceProperties();
         properties.setBaseUrl("http://auth-service");
+        properties.setInternalServiceToken("token-123");
 
         RestPersonnelDirectory directory = new RestPersonnelDirectory(builder, properties);
 
         server.expect(requestTo("http://auth-service/internal/users/11/identity"))
                 .andExpect(method(HttpMethod.GET))
+                .andExpect(header("X-Internal-Service-Token", "token-123"))
                 .andRespond(withSuccess("""
                         {"id":11,"email":"mandor@mysawit.id","nama":"Mandor","role":"MANDOR"}
                         """, MediaType.APPLICATION_JSON));
@@ -68,16 +74,88 @@ class RestPersonnelDirectoryTest {
 
         AuthServiceProperties properties = new AuthServiceProperties();
         properties.setBaseUrl("http://auth-service");
+        properties.setInternalServiceToken("token-123");
 
         RestPersonnelDirectory directory = new RestPersonnelDirectory(builder, properties);
 
         server.expect(requestTo("http://auth-service/internal/users/99/identity"))
                 .andExpect(method(HttpMethod.GET))
+                .andExpect(header("X-Internal-Service-Token", "token-123"))
                 .andRespond(withStatus(NOT_FOUND));
 
         IllegalArgumentException ex = assertThrows(IllegalArgumentException.class,
                 () -> directory.requireMandorId("99"));
 
         assertEquals("MANDOR user not found: 99", ex.getMessage());
+    }
+
+    @Test
+    void requireSupirIdShouldReturnCanonicalNumericIdentifierWhenUserRoleMatches() {
+        RestClient.Builder builder = RestClient.builder();
+        MockRestServiceServer server = MockRestServiceServer.bindTo(builder).build();
+
+        AuthServiceProperties properties = new AuthServiceProperties();
+        properties.setBaseUrl("http://auth-service");
+        properties.setInternalServiceToken("token-123");
+
+        RestPersonnelDirectory directory = new RestPersonnelDirectory(builder, properties);
+
+        server.expect(requestTo("http://auth-service/internal/users/5/identity"))
+                .andExpect(method(HttpMethod.GET))
+                .andExpect(header("X-Internal-Service-Token", "token-123"))
+                .andRespond(withSuccess("""
+                        {"id":5,"email":"supir@mysawit.id","nama":"Supir","role":"SUPIR"}
+                        """, MediaType.APPLICATION_JSON));
+
+        String canonicalId = directory.requireSupirId("5");
+
+        assertEquals("5", canonicalId);
+        server.verify();
+    }
+
+    @Test
+    void requireMandorIdShouldThrowIllegalStateWhenAuthServiceFails() {
+        RestClient.Builder builder = RestClient.builder();
+        MockRestServiceServer server = MockRestServiceServer.bindTo(builder).build();
+
+        AuthServiceProperties properties = new AuthServiceProperties();
+        properties.setBaseUrl("http://auth-service");
+        properties.setInternalServiceToken("token-123");
+
+        RestPersonnelDirectory directory = new RestPersonnelDirectory(builder, properties);
+
+        server.expect(requestTo("http://auth-service/internal/users/7/identity"))
+                .andExpect(method(HttpMethod.GET))
+                .andExpect(header("X-Internal-Service-Token", "token-123"))
+                .andRespond(withStatus(INTERNAL_SERVER_ERROR));
+
+        IllegalStateException ex = assertThrows(IllegalStateException.class,
+                () -> directory.requireMandorId("7"));
+
+        assertEquals("Failed to validate MANDOR identity", ex.getMessage());
+    }
+
+    @Test
+    void requireMandorIdShouldThrowIllegalStateWhenIdentityPayloadIsIncomplete() {
+        RestClient.Builder builder = RestClient.builder();
+        MockRestServiceServer server = MockRestServiceServer.bindTo(builder).build();
+
+        AuthServiceProperties properties = new AuthServiceProperties();
+        properties.setBaseUrl("http://auth-service");
+        properties.setInternalServiceToken("token-123");
+
+        RestPersonnelDirectory directory = new RestPersonnelDirectory(builder, properties);
+
+        server.expect(requestTo("http://auth-service/internal/users/7/identity"))
+                .andExpect(method(HttpMethod.GET))
+                .andExpect(header("X-Internal-Service-Token", "token-123"))
+                .andRespond(withSuccess("""
+                        {"id":7,"email":"mandor@mysawit.id","nama":"Mandor"}
+                        """, MediaType.APPLICATION_JSON));
+
+        IllegalStateException ex = assertThrows(IllegalStateException.class,
+                () -> directory.requireMandorId("7"));
+
+        assertEquals("Failed to validate MANDOR identity", ex.getMessage());
     }
 }
